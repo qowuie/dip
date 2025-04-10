c2fcd049e74a3f0860121b3b5ae9be3558dcf19c
pavel <PASHA12369@yandex.ru>
2025-04-10 22:42:02 +0300

    Доработка фронта, вывод доп информации в JSON

--- marketing_site/api/serializers.py
+++ marketing_site/api/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 from accounts.models import CustomUser
 from orders.models import Order
+from main.models import Service
+
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = ['id', 'name', 'description', 'price']
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,6 +19,7 @@ class UserSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     client = UserSerializer(read_only=True)
     executor = UserSerializer(read_only=True)
+    service = ServiceSerializer(read_only=True)
 
     class Meta:
         model = Order
